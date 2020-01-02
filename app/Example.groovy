c7bfbd83fcc893c3a1871ae7af3b3d303d47c563
Jin Cheng <cdjc@cn.ibm.com>
2020-01-02 15:31:12 +0800

    hello groovy

---
+++ app/Example.groovy
@@ -0,0 +1,9 @@
+class Example {
+    static void main(String[] args) {
+        // Using a simple println statement to print output to the console
+        println('Hello Groovy World, Javier!');
+        def range = 5..10; 
+        println(range.toList()); 
+        println(range.get(2));     
+    }
+}
